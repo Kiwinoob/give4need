71c9604f10985c8315c40a3ff9b3b9b56891a84e
Kiwinoob <Kiwi1@live.com.sg>
2024-09-03 16:00:23 +0800

    rename page name

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   description: "Login to your account",
 };
 
-export default function Page() {
+export default function LoginPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
